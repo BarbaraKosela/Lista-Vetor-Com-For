0fafc2f195bf770bde1fe91b32a4d9c2bc6e9fd6
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-04-27 13:44:20 -0300

    Inicialização de atividade
    Conclusão da primeira atividade de lista de exercícios misturando for e vetores, com posições e armazenamento de informações.

---
+++ ListaVetorComFor/ListaVetorComFor/Exercicio01.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ListaVetorComFor
+{
+    class Exercicio01
+    {
+        public Exercicio01()
+        {
+            string[] nomes = new string[25];
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.Write("Nome: ");
+                nomes[i] = Console.ReadLine();
+            }
+        }
+    }
+}
